18ddaffb111e6fdf01803af61e2811d4e326ff9e
James Aley <james.aley@gmail.com>
2011-04-30 14:47:57 +0100

    Prevent custom all over the face.

--- init.el
+++ init.el
@@ -1,13 +1,16 @@
 ;; Main emacs init script, call other sub-scripts from here.
 
-
 ;; Directory where sub scripts are kept
 (defvar *emacs-cfg-dir* "~/.emacs.d")
+
+;; Protect the init file from custom.
+(setq custom-file (concat (file-name-as-directory *emacs-cfg-dir*) "custom.el"))
+(load custom-file 'noerror)
 
 ;; List of sub-scripts to be loaded on initialization
 (defvar *scripts*
   '("general"))
 
 (mapcar (lambda (el)
-	  (load (concat (file-name-as-directory *emacs-cfg-dir*) el))) 
+	  (load (concat (file-name-as-directory *emacs-cfg-dir*) el)  'noerror)) 
 	*scripts*)
