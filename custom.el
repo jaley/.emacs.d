37bc0df89800af3941a565e981b59ea30ef51b1d
James Aley <james.aley@gmail.com>
2011-05-01 11:13:52 +0100

    Added font configuration to custom file. Ignoring session files in version control.

---
+++ custom.el
@@ -0,0 +1,12 @@
+;; This file is full of variables set by custom.
+;; hand-editing is not recommended.
+
+(custom-set-faces
+ '(default 
+    ((t (:inherit nil 
+		  :stipple nil :background "#fcf4dc" 
+		  :foreground "#52676f" :inverse-video nil 
+		  :box nil :strike-through nil :overline nil 
+		  :underline nil :slant normal :weight normal 
+		  :height 98 :width normal :foundry "unknown" 
+		  :family "DejaVu Sans Mono")))))
