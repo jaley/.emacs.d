d4f2254515437cc46e6a3616fbc86d1c7e812d05
James Aley <james.aley@gmail.com>
2011-05-04 15:41:24 +0100

    Moved color theme solarized into new plugins folder.

--- theme.el
+++ theme.el
@@ -2,6 +2,6 @@
 
 (require 'color-theme)
 
-(add-to-list 'load-path "~/.emacs.d/emacs-color-theme-solarized")
+(add-to-list 'load-path "~/.emacs.d/plugins/emacs-color-theme-solarized")
 (require 'color-theme-solarized)
 (color-theme-solarized-light)
