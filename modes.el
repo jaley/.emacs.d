449a93cc20f2258f67ce84a3eadd539ddd8aa98a
James Aley <james.aley@gmail.com>
2011-07-03 21:54:57 +0100

    Fore some reason, full path needed.

--- modes.el
+++ modes.el
@@ -4,10 +4,11 @@
 (require 'android-mode)
 
 ;; Lua mode
-(add-to-list 'load-path "plugins/lua-mode")
+(add-to-list 'load-path (file-name-as-directory "/home/jaley/.emacs.d/plugins/lua-mode"))
 (require 'lua-mode)
 
 ; get lua-mode to handle lua files
-(add-to-list 'auto-mode-alist
-             '("\\.lua$" . lua-mode))
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
 (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
